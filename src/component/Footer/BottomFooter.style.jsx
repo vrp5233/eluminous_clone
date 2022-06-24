3ee3b0a1b31f4524791771e64612a40f02739cab
vrp5233 <eluminous.design11@gmail.com>
2022-06-24 20:09:24 +0530

    Updated Footer

---
+++ src/component/Footer/BottomFooter.style.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { GrFacebookOption } from "react-icons/gr";
+
+export const BottomeFooter = (props) => {
+  return (
+    <>
+      <GrFacebookOption />
+    </>
+  );
+};
+
+export default BottomeFooter;
